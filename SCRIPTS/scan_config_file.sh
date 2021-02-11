47deff73fe12e9e4318eb1c10e6df6bb5b17f619
AVGaidai <diligent20494@gmail.com>
2021-02-11 19:59:19 +0700

    Refactored; modified/added describes and some warning messages

--- SCRIPTS/scan_config_file.sh
+++ SCRIPTS/scan_config_file.sh
@@ -3,14 +3,16 @@
 #####################################################################################
 # @author Anatoly Gaidai (see avgaidai.github.io)                                   #
 #                                                                                   #
-# This script gets all useful parameters from specified config file (excluding      #
-# line comment sterting with '#' character).                                        #
+# This script gets useful content from config file (excluding line comment          #
+# sterting with '#' character).                                                     #
 #                                                                                   #
-# Argument 1 -- $1 is path to config file (including his full name).                #
+# Input                                                                             #
+# Arg 1: 'CONFIG_FILE' is path to config file.                                      #
 #                                                                                   #
-# Script returns 0 and prints out all useful parameters obtained from the specified #
-# config file if the script is successfully executed, or prints out (to stderr)     #
-# error message and returns one of the following non-zero error codes:              #
+# Output                                                                            #
+# Script returns 0 and prints out useful content from specified                     #
+# config file if script is successfully executed, or prints out (to stderr)         #
+# error message and returns one of following non-zero error codes:                  #
 # 1 -- not enough script arguments;                                                 #
 # 2 -- specified file is not exist.                                                 #
 #                                                                                   #
@@ -30,39 +32,40 @@ FONT_GREEN="\033[32m"
 END_FORMATTING="\033[0m"
 
 
-# Checking the number of script arguments
+# Checking number of script arguments
 if [[ $1 == "" ]]
 then
 
     echo -e " [$FONT_BOLD$FONT_YELLOW WARNING $END_FORMATTING]:" \
          "$FONT_BOLD$FONT_RED $0$END_FORMATTING:\n" \
          "\t- Not enough script arguments!\n" \
-         "\t- Script launch format:$FONT_BOLD$FONT_GREEN $0 CONFIG_FILE_NAME$END_FORMATTING\n" 1>&2
+         "\t- Script launch format:$FONT_BOLD$FONT_GREEN $0" \
+         "CONFIG_FILE$END_FORMATTING\n" \
+         "\t $FONT_GREEN CONFIG_FILE is path to config file" 1>&2
          
     exit 1
     
 fi
 
-# Path to the config file
 CONFIG_FILE=$1
 
-
-# Checking if a config file exists
+# Checking if config file exists
 if [[ ! -f $CONFIG_FILE ]]
 then
 
     echo -e " [$FONT_BOLD$FONT_YELLOW WARNING $END_FORMATTING]:" \
          "$FONT_BOLD$FONT_RED $0$END_FORMATTING:\n" \
-         "\t- File$FONT_ITALIC$FONT_BLUE$CONFIG_FILE$END_FORMATTING is not exist!\n" 1>&2 
+         "\t- No such config file $FONT_YELLOW$CONFIG_FILE$END_FORMATTING!\n" 1>&2 
 
     exit 2
         
 fi
 
 
-# Printing all useful parameters from the config file
+# Printing useful content from config file
 RESULT=$(cat $CONFIG_FILE | sed "s/\#.*//g" | sed "/^[ \t]*$/d")
 
+# Removing extra spaces
 echo $RESULT
 
 exit 0
